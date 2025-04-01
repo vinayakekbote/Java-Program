8223efc8fffdaaf1bcd5d90418ea529711df59c6
vinayakekbote <vinayakekbote12@gmail.com>
2025-04-02 00:00:08 +0530

    First Practices

--- src/com/BinarySearch/BinarySearch.java
+++ src/com/BinarySearch/BinarySearch.java
@@ -2,6 +2,23 @@ package com.BinarySearch;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int arr[] = {1, 3, 5, 6, 8};
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        int element = 6;
+
+        while (start <= end) {
+            int mid = (start+end)/2;
+            if (element == arr[mid]) {
+                System.out.println("Element found in index = " + mid);
+                break;
+            } else if (element > arr[mid]) {
+                start = mid + 1;
+            } else if (element < arr[mid]) {
+                end = mid - 1;
+            }
+        }
     }
 }
