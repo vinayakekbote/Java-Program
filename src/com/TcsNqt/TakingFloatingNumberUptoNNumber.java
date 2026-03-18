c5d490d3722f70bcec55c05afc9df633519d3791
vinayakekbote <vinayakekbote12@gmail.com>
2026-03-18 07:42:34 +0530

    changes new practice added

---
+++ src/com/TcsNqt/TakingFloatingNumberUptoNNumber.java
@@ -0,0 +1,15 @@
+package com.TcsNqt;
+import java.util.*;
+
+public class TakingFloatingNumberUptoNNumber {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        float n= sc.nextFloat();
+
+
+
+        System.out.printf("option1 1 = "+"%.2f", n);
+        System.out.println("option1 2 = "+String.format("%.2f", n));
+
+    }
+}
