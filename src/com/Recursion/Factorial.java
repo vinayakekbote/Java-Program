fef038a78f356df6e506df5c5c7c6ffddde1e57e
vinayakekbote <vinayakekbote12@gmail.com>
2026-05-10 10:13:48 +0530

    changes new practice added

--- src/com/Recursion/Factorial.java
+++ src/com/Recursion/Factorial.java
@@ -1,4 +1,15 @@
 package com.Recursion;
 
 public class Factorial {
+    public static void main(String[] args) {
+        System.out.print(fibo(5));
+    }
+
+    private static int fibo(int n) {
+        if(n<=1){
+            return n;
+        }
+
+        return fibo(n-1) + fibo(n-2);
+    }
 }
