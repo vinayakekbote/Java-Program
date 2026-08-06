f997246a3dea530c38ce1dbd1b7e99967801bd78
vinayakekbote <vinayakekbote12@gmail.com>
2026-08-06 08:07:09 +0530

    binary search added

--- src/com/LeetCode/Easy/MoveZeros_283.java
+++ src/com/LeetCode/Easy/MoveZeros_283.java
@@ -10,7 +10,7 @@ public class MoveZeros_283 {
 
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                nums[count++]=nums[i];
+                nums[count++] = nums[i];
             }
         }
 
