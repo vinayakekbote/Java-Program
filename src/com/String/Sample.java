8070121f4fc393f8bb8ab3436952363d4310bc68
vinayakekbote <vinayakekbote12@gmail.com>
2026-08-07 19:40:01 +0530

    practice added

--- src/com/String/Sample.java
+++ src/com/String/Sample.java
@@ -2,7 +2,7 @@ package com.String;
 
 public class Sample {
     public static void main(String[] args) {
-        final String s="Vinayak";
+        final String s = "Vinayak";
         System.out.println(s);
     }
 }
